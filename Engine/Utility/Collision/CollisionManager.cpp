e75dec34783b1a721757f7bcfc676e1c1ac93939
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2025-03-28 16:46:19 +0900

    関数の位置変更

--- Engine/Utility/Collision/CollisionManager.cpp
+++ Engine/Utility/Collision/CollisionManager.cpp
@@ -25,76 +25,7 @@ CollisionManager* CollisionManager::GetInstance()
 	return &instance;
 }
 
-void CollisionManager::Initialize() {
-	isDrawCollider_ = false;
-}
 
-void CollisionManager::Update()
-{
-	CheckAllCollisions();
-
-}
-
-
-
-void CollisionManager::Reset() {
-	// リストを空っぽにする
-	colliders_.clear();
-
-}
-
-void CollisionManager::CheckCollisionPair(Collider* colliderA, Collider* colliderB) {
-	// コライダーAの座標を取得
-	Vector3 PosA = colliderA->GetCenterPosition();
-	// コライダーBの座標を取得
-	Vector3 PosB = colliderB->GetCenterPosition();
-	// 座標の差分ベクトル
-	Vector3 subtract = PosB - PosA;
-	// 座標AとBの距離を求める
-	float distance = Length(subtract);
-
-	// 球と球の交差判定
-	//if (distance <= (colliderA->GetRadius() + colliderB->GetRadius())) {
-	//	// コライダーAの衝突時コールバックを呼び出す
-	//	colliderA->OnCollision(colliderB);
-	//	// コライダーBの衝突時コールバックを呼び出す
-	//	colliderB->OnCollision(colliderA);
-	//}
-}
-
-void CollisionManager::CheckAllCollisions() {
-	// リスト内のペアを総当たり
-	std::list<Collider*>::iterator itrA = colliders_.begin();
-	for (; itrA != colliders_.end(); ++itrA) {
-		Collider* colliderA = *itrA;
-
-		// イテレーターBはイテレーターAの次の要素から回す（重複判定を回避）
-		std::list<Collider*>::iterator itrB = itrA;
-		itrB++;
-
-		for (; itrB != colliders_.end(); ++itrB) {
-			Collider* colliderB = *itrB;
-
-			// ペアの当たり判定
-			CheckCollisionPair(colliderA, colliderB);
-		}
-	}
-}
-
-
-
-void CollisionManager::AddCollider(Collider* collider) {
-	if (!collider) return;
-	colliders_.push_back(collider);
-	std::cout << "Collider added: " << collider->GetTypeID() << std::endl;
-}
-
-void CollisionManager::RemoveCollider(Collider* collider)
-{
-	if (!collider) return;
-	colliders_.remove(collider);
-	std::cout << "Collider removed: " << collider->GetTypeID() << std::endl;
-}
 
 bool Collision::Check(const SphereCollider* a, const SphereCollider* b)
 {
@@ -206,4 +137,78 @@ bool Collision::Check(const AABBCollider* aabb, const OBBCollider* obb)
 bool Collision::Check(const OBBCollider* a, const OBBCollider* b)
 {
 	return Check(a->GetOBB(), b->GetOBB());
+}
+
+
+
+
+void CollisionManager::Initialize() {
+	isDrawCollider_ = false;
+}
+
+void CollisionManager::Update()
+{
+	CheckAllCollisions();
+
+}
+
+
+
+void CollisionManager::Reset() {
+	// リストを空っぽにする
+	colliders_.clear();
+
+}
+
+void CollisionManager::CheckCollisionPair(Collider* colliderA, Collider* colliderB) {
+	// コライダーAの座標を取得
+	Vector3 PosA = colliderA->GetCenterPosition();
+	// コライダーBの座標を取得
+	Vector3 PosB = colliderB->GetCenterPosition();
+	// 座標の差分ベクトル
+	Vector3 subtract = PosB - PosA;
+	// 座標AとBの距離を求める
+	float distance = Length(subtract);
+
+	// 球と球の交差判定
+	//if (distance <= (colliderA->GetRadius() + colliderB->GetRadius())) {
+	//	// コライダーAの衝突時コールバックを呼び出す
+	//	colliderA->OnCollision(colliderB);
+	//	// コライダーBの衝突時コールバックを呼び出す
+	//	colliderB->OnCollision(colliderA);
+	//}
+}
+
+void CollisionManager::CheckAllCollisions() {
+	// リスト内のペアを総当たり
+	std::list<Collider*>::iterator itrA = colliders_.begin();
+	for (; itrA != colliders_.end(); ++itrA) {
+		Collider* colliderA = *itrA;
+
+		// イテレーターBはイテレーターAの次の要素から回す（重複判定を回避）
+		std::list<Collider*>::iterator itrB = itrA;
+		itrB++;
+
+		for (; itrB != colliders_.end(); ++itrB) {
+			Collider* colliderB = *itrB;
+
+			// ペアの当たり判定
+			CheckCollisionPair(colliderA, colliderB);
+		}
+	}
+}
+
+
+
+void CollisionManager::AddCollider(Collider* collider) {
+	if (!collider) return;
+	colliders_.push_back(collider);
+	std::cout << "Collider added: " << collider->GetTypeID() << std::endl;
+}
+
+void CollisionManager::RemoveCollider(Collider* collider)
+{
+	if (!collider) return;
+	colliders_.remove(collider);
+	std::cout << "Collider removed: " << collider->GetTypeID() << std::endl;
 }
