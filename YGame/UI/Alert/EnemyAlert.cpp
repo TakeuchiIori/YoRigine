5157de98376a9d1eb04bf0146d8372ac77929679
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2026-03-09 12:38:01 +0900

    敵UIの作成を一旦ストップ

--- YGame/UI/Alert/EnemyAlert.cpp
+++ YGame/UI/Alert/EnemyAlert.cpp
@@ -1,1 +1,20 @@
 #include "EnemyAlert.h"
+#include "Systems/UI/UIManager.h"
+
+
+void EnemyAlert::Initialize() {
+	alertUI_ = YoRigine::UIManager::GetInstance()->GetUI("enemyAlert");
+	if (alertUI_) {
+		defaultScale_ = alertUI_->GetScale();
+	}
+}
+
+void EnemyAlert::Update() {
+
+}
+
+void EnemyAlert::Draw() {
+	if (alertUI_) {
+		alertUI_->Draw();
+	}
+}
--- YGame/UI/Alert/EnemyAlert.h
+++ YGame/UI/Alert/EnemyAlert.h
@@ -1,5 +1,19 @@
 #pragma once
+#include "Systems/UI/UIBase.h"
 class EnemyAlert
 {
+
+public:
+
+	void Initialize();
+
+	void Update();
+
+	void Draw();
+
+private:
+
+	UIBase* alertUI_ = nullptr;
+	Vector2 defaultScale_{};
 };
 
